56978be2f3830af77a93dd740cf06e21f1b34a4e
Eduardo Gutiérrez <eduardo.gutierrez.2325@gmail.com>
2019-02-15 13:22:04 -0600

    Adding basic styling to Person component and using basic conditional rendering in JSX

--- src/App.js
+++ src/App.js
@@ -7,30 +7,55 @@ class App extends Component {
     people: [
       { name: 'Eduardo', age: 27 },
       { name: 'Juan', age: 30 }
-    ]
+    ],
+    showPeople: false
   }
 
-  switchNameHandler = () => {
-    console.log('Before', this.state.people);
-    this.setState(() => {
+  togglePeopleHandler = () => {
+    this.setState(prevState => {
       return {
-        people: [
-          { name: 'Luis', age: 30 },
-          { name: 'Juan', age: 21 } 
-        ]
-      };
-    })
+        showPeople: !prevState.showPeople
+      }
+    });
+  };
 
-    console.log(this.state.people);
-  }
+  nameChangeHandler = (e) => {
+    const newName = e.target.value;
+
+    if (newName) {
+      this.setState(() => {
+        return {
+          people: [
+            { name: newName, age: 30 },
+            { name: 'Juan', age: 21 } 
+          ]
+        }
+      })
+    }
+  };
+  
+  logMessage = (dataFromChild) => {
+    console.log(dataFromChild);
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Click me plx</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
-        <Person name={this.state.people[1].name} age={this.state.people[1].age} />
+        <button className="App-button" onClick={this.togglePeopleHandler}>Click me</button>
+        {this.state.showPeople && <div>
+          <Person 
+            name={this.state.people[0].name} 
+            age={this.state.people[0].age}
+            logMessage={() => this.logMessage('Argument from child component')} 
+          />
+          <Person 
+            name={this.state.people[1].name} 
+            age={this.state.people[1].age}
+            logMessage={this.logMessage.bind(this, 'Argument from second child')}
+            nameChangeHandler={this.nameChangeHandler}
+          />
+        </div>}
       </div>
     );
   }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
-    return <p>Hi, I'm {props.name} and I'm {props.age} year(s) old</p>; 
+    return (
+        <div className="Person">
+            <p onClick={props.logMessage}>Hi, I'm {props.name} and I'm {props.age} year(s) old</p>
+            <p>{props.children}</p>
+            <input type="text" name="newName" onChange={props.nameChangeHandler} value={props.name}/>
+        </div>
+    );
 };
 
 export default person;
